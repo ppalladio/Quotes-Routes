570b096a67bca79ff0e3842376fc48b52c0f84cd
Yuxuan <nine.slide@gmail.com>
2022-10-24 17:27:41 +0200

    add useParams

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,5 +1,13 @@
+import { useParams } from 'react-router-dom';
+
 const NewQuote = () => {
-    return <h1> new quote</h1>
-}
+    const params = useParams();
+    return (
+        <>
+            <h1> new quote</h1>
+            <p>{params.quoteId}</p>
+        </>
+    );
+};
 
 export default NewQuote;
