73145ecd06b45573fca4909d38e9e5b38eb0483d
Yuxuan <nine.slide@gmail.com>
2022-10-27 15:40:08 +0200

    lazy loading!!

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
+import React, { Suspense } from 'react';
+
 import { Route, Switch, Redirect } from 'react-router-dom';
 import AllQuotes from './pages/AllQuotes';
-import NewQuote from './pages/NewQuote';
-import QuoteDetail from './pages/QuoteDetails';
 import Layout from './components/layout/Layout';
-import NotFound from './pages/NotFound';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetails'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
+//'this page will only be downloaded when needed
+
 function App() {
     return (
         <Layout>
-            <Switch>
-                <Route path="/" exact>
-                    <Redirect to="/quotes" />
-                </Route>
-                <Route path="/quotes" exact>
-                    <AllQuotes />
-                </Route>
-                <Route path="/quotes/:quoteId">
-                    <QuoteDetail />
-                </Route>
-                <Route path="/new-quote">
-                    <NewQuote />
-                </Route>
-                <Route path="*">
-                    {/* //' path=* wiith match all the pages, so it has to be put on the past to math unfound pages */}
-                    <NotFound />
-                </Route>
-            </Switch>
+            <Suspense
+                fallback={
+                    <div className="centered">
+                        <LoadingSpinner />
+                    </div>
+                }
+            >
+                <Switch>
+                    <Route path="/" exact>
+                        <Redirect to="/quotes" />
+                    </Route>
+                    <Route path="/quotes" exact>
+                        <AllQuotes />
+                    </Route>
+                    <Route path="/quotes/:quoteId">
+                        <QuoteDetail />
+                    </Route>
+                    <Route path="/new-quote">
+                        <NewQuote />
+                    </Route>
+                    <Route path="*">
+                        {/* //' path=* wiith match all the pages, so it has to be put on the past to math unfound pages */}
+                        <NotFound />
+                    </Route>
+                </Switch>
+            </Suspense>
         </Layout>
     );
 }
